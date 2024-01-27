0e0f0808a9c604213946ce4a6c7356bb52150c5b
Aaron Lott <aaron@ezeducation.co.uk>
2024-01-27 20:52:45 +0000

    Fix player and camera movement

--- Assets/Scripts/WaiterWalkMovement.cs
+++ Assets/Scripts/WaiterWalkMovement.cs
@@ -9,71 +9,40 @@ public class WaiterWalkMovement : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private Transform cameraLookAtTarget;
     [SerializeField] private Transform cameraPositionTarget;
-   [SerializeField] private WaiterWalkingCamera walkingCamera;
 
     [Header("Properties")]
     [Range(0f, 1f)] [SerializeField] private float drunkness;
-    [SerializeField] float runSpeed = 3.5f;
-    [SerializeField] float walkSpeed = 1f;
+    [SerializeField] private float rotationSpeed = 80f;
+    [SerializeField] private float runSpeed = 3.5f;
+    [SerializeField] private float walkSpeed = 1f;
 
-    private float smoothVelocity;
-    private readonly float smoothTime = 0.1f;
-
-    public Transform CameraLookAtTarget => cameraLookAtTarget;
-    public Transform CameraPositionTarget => cameraPositionTarget;
-
-    public Vector2 WalkDirection { get; private set; }
+    public Vector2 MoveDirection { get; private set; }
     public float Drunkness => drunkness;
     public float MoveSpeed => ((runSpeed - walkSpeed) * drunkness) + walkSpeed;
 
-
     private void Update()
     {
-        MovementHandler();
-    }
+        // Rotate the player based on input
+        transform.Rotate(Vector3.up, MoveDirection.x * rotationSpeed * Time.deltaTime);
 
-    public void MovementHandler()
-    {
-        // Calculate the movement direction.
-        Vector3 movement = new Vector3(WalkDirection.x, 0f, WalkDirection.y).normalized;
+        // Get the forward direction of the player
+        Vector3 forwardDirection = transform.forward;
 
         animator.SetFloat("speed", drunkness);
-        animator.SetBool("isMoving", movement.magnitude > 0.1f);
+        animator.SetBool("isMoving", MoveDirection.magnitude > 0.1f);
 
-        Vector3 qwe = walkingCamera.transform.forward;
-        Vector3 rty = walkingCamera.transform.right;
+        // Get vertical input for movement
+        float verticalInput = Input.GetAxis("Vertical");
 
-        qwe.y = 0;
-        rty.y = 0;
+        // Calculate movement in the forward direction relative to the player's rotation
+        Vector3 moveAmount = forwardDirection * verticalInput * MoveSpeed * Time.deltaTime;
 
-        qwe.Normalize();
-        rty.Normalize();
-
-        Vector3 test1 = qwe * WalkDirection.y;
-        Vector3 test2 = rty * WalkDirection.x;
-
-        Vector3 test3 = test1 + test2;
-
-        // Check if there is any input, then move the character.
-        if (movement.magnitude > 0.1f)
-        {
-            // Calculate the rotation angle based on the input.
-            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothVelocity, smoothTime);
-
-            // Apply rotation to the character
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            // Calculate the movement direction in world space.
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-
-            // Move the character.
-            transform.Translate(test3 * MoveSpeed * Time.deltaTime, Space.World);
-        }
+        // Translate the player in the calculated direction
+        transform.Translate(moveAmount, Space.World);
     }
 
     public void OnWalkingMovement(InputValue value)
     {
-        WalkDirection = value.Get<Vector2>();
+        MoveDirection = value.Get<Vector2>();
     }
 }
